9ba837ae7d5f37097cab15c1293576626a0779f7
iiKingKai <91346761+iiKingKai@users.noreply.github.com>
2023-04-06 15:17:29 -0500

    Update ConfigUtils.java
    Upgrade Changes

--- src/main/java/me/tomisanhues2/pmines/utils/ConfigUtils.java
+++ src/main/java/me/tomisanhues2/pmines/utils/ConfigUtils.java
@@ -10,9 +10,33 @@ public class ConfigUtils {
 
     public ConfigUtils() {
         upgradeLevels = List.of(
-                new UpgradeLevel(1, 100, List.of("DIRT")),
-                new UpgradeLevel(2, 200, List.of("DIRT", "STONE")),
-                new UpgradeLevel(3, 300, List.of("DIRT", "STONE", "COBBLESTONE"))
+                new UpgradeLevel(1, 1000000, List.of("COBBLESTONE")),
+                new UpgradeLevel(2, 1000000, List.of("COBBLESTONE", "STONE")),
+                new UpgradeLevel(3, 1000000, List.of("COBBLESTONE", "STONE", "STONE_BRICKS")),
+                new UpgradeLevel(4, 5000000, List.of("ANDESITE")),
+                new UpgradeLevel(5, 5000000, List.of("ANDESITE", "GRANITE")),
+                new UpgradeLevel(6, 5000000, List.of("ANDESITE", "GRANITE", "DIORITE")),
+                new UpgradeLevel(7, 10000000, List.of("POLISHED_ANDESITE")),
+                new UpgradeLevel(8, 10000000, List.of("POLISHED_ANDESITE", "POLISHED_GRANITE")),
+                new UpgradeLevel(9, 10000000, List.of("POLISHED_ANDESITE", "POLISHED_GRANITE", "POLISHED_DIORITE")),
+                new UpgradeLevel(10, 15000000, List.of("BLACKSTONE")),
+                new UpgradeLevel(11, 15000000, List.of("BLACKSTONE", "POLISHED_BLACKSTONE")),
+                new UpgradeLevel(12, 15000000, List.of("BLACKSTONE", "POLISHED_BLACKSTONE", "GILDED_BLACKSTONE")),
+                new UpgradeLevel(13, 20000000, List.of("PRISMARINE")),
+                new UpgradeLevel(14, 25000000, List.of("PRISMARINE", "PRISMARINE_BRICKS")),
+                new UpgradeLevel(15, 30000000, List.of("PRISMARINE", "PRISMARINE_BRICKS", "DARK_PRISMARINE")),
+                new UpgradeLevel(16, 35000000, List.of("DEEPSLATE")),
+                new UpgradeLevel(17, 40000000, List.of("DEEPSLATE", "DEEPSLATE_TILES")),
+                new UpgradeLevel(18, 45000000, List.of("DEEPSLATE", "DEEPSLATE_TILES", "DEEPSLATE_DIAMOND_ORE")),
+                new UpgradeLevel(19, 50000000, List.of("DEEPSLATE", "DEEPSLATE_TILES", "DEEPSLATE_EMERALD_ORE")),
+                new UpgradeLevel(20, 60000000, List.of("MOSSY_COBBLESTONE")),
+                new UpgradeLevel(21, 70000000, List.of("MOSSY_COBBLESTONE", "MOSSY_STONE_BRICKS")),
+                new UpgradeLevel(22, 80000000, List.of("MOSSY_COBBLESTONE", "MOSSY_STONE_BRICKS", "CHISELED_STONE_BRICKS")),
+                new UpgradeLevel(23, 90000000, List.of("NETHERRACK")),
+                new UpgradeLevel(24, 100000000, List.of("NETHERRACK", "NETHER_BRICKS")),
+                new UpgradeLevel(25, 110000000, List.of("NETHERRACK", "NETHER_BRICKS", "NETHER_QUARTZ_ORE")),
+                new UpgradeLevel(26, 120000000, List.of("END_STONE")),
+                new UpgradeLevel(27, 130000000, List.of("END_STONE", "END_STONE_BRICKS")),
         );
     }
 
